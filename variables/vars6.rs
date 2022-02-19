be4201113c57856979137dcab968d7b03f3f09a6
Krypto Kiddo <97212160+krypto-kiddo@users.noreply.github.com>
2022-02-19 23:14:10 +0530

    Update vars6.rs

--- variables/vars6.rs
+++ variables/vars6.rs
@@ -6,3 +6,9 @@ fn main(){
 }
 
 // Still can't figure out why is it better to name constants as upper case...
+
+// Update: Found something. 
+// Its more of a habbit than of any technical use.
+// This comes from naming constants as UPERCASE in C#
+// Which, actually comes from the habbit of naming preprocessor macros in upper case. 
+// They did it because reading preprocessor macros was not really easy. 
