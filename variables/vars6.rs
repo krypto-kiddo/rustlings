589c51c391b11cf3a7ff86d251511b06d041f6c7
Krypto Kiddo <97212160+krypto-kiddo@users.noreply.github.com>
2022-02-19 23:10:28 +0530

    Create vars6.rs

---
+++ variables/vars6.rs
@@ -0,0 +1,8 @@
+// Code by krypto kiddo
+// Variables Excercise 6 : Declaring Constants
+const NUMBER: i32 = 3;
+fn main(){
+    println!("Number is {}",NUMBER);
+}
+
+// Still can't figure out why is it better to name constants as upper case...
