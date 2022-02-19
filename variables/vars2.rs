2f71ff8bd3014a96002ba8c1385af4f7c8d7db70
Krypto Kiddo <97212160+krypto-kiddo@users.noreply.github.com>
2022-02-19 22:58:27 +0530

    Create vars2.rs

---
+++ variables/vars2.rs
@@ -0,0 +1,11 @@
+// Code by Krypto Kiddo
+// Vars excercise 2 - defining variables and if else statement
+
+fn main() {
+    let x = 10;
+    if x == 10 {
+        println!("It is ten!");
+    } else {
+        println!("No Not ten...");
+    }
+}
