499af352297ea31b9941957b5bcd8237c0bb2395
Krypto Kiddo <97212160+krypto-kiddo@users.noreply.github.com>
2022-02-19 23:06:53 +0530

    Create vars5.rs

---
+++ variables/vars5.rs
@@ -0,0 +1,8 @@
+// Code by Krypto Kiddo
+// Varaibles Excercise 5 : Changing var type and addition
+fn main() {
+    let number = "THREE";
+    println!("Number Spells as {}",number);
+    let number = 3;
+    println!("The number plus 2 is {}",number+2);
+}
