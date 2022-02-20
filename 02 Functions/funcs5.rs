a3071965a30f4b9c5b01408aa4bf05cea4efc6a2
Krypto Kiddo <97212160+krypto-kiddo@users.noreply.github.com>
2022-02-20 17:59:37 +0530

    Create funcs5.rs

---
+++ 02 Functions/funcs5.rs
@@ -0,0 +1,12 @@
+// Code by Krypto Kiddo
+// Functions Ex5 : The semicolon problem
+fn main(){
+    let ans = square(3);
+    println!("The answer is {}",ans);
+}
+
+fn square(num:i32)->i32{
+    num*num
+}
+
+// Basically the same problem i already discovered in the previous one. Whew 
