bc0ce96acc3062ed7f2c0c3ac24f0d2a5bb468da
Krypto Kiddo <97212160+krypto-kiddo@users.noreply.github.com>
2022-03-06 23:36:38 +0530

    Create pt03.rs

---
+++ 05 Primitive Types/pt03.rs
@@ -0,0 +1,13 @@
+// Code by Krypto Kiddo
+// Primitive Types Ex 3 : Array Type
+
+fn main(){
+    let a = [0,1,2,3,4,5];
+    if a.len() >= 100{
+        println!("Woah, that's one huge array");
+    }
+    else{
+        println!("bruh, I eat such arrays for breakfast!");
+    }
+}
+
