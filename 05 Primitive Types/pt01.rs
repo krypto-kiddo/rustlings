11e6abcd7e99b7b1b928e20eb42c16cb6079dad8
Krypto Kiddo <97212160+krypto-kiddo@users.noreply.github.com>
2022-03-06 23:14:53 +0530

    Create pt01.rs

---
+++ 05 Primitive Types/pt01.rs
@@ -0,0 +1,13 @@
+// Code by Krypto Kiddo
+// Primitive Types Ex 1 : Bool with If and Else 
+
+fn main(){
+    let is_morning = true;
+    if is_morning{
+        println!("Good Morning!");
+    }
+    let is_evening = false;
+    if is_evening{
+        println!("Good Evening!");
+    }
+}
