00a2f82136dcf1f60b1bf68b91bdf41aacd20a56
Krypto Kiddo <97212160+krypto-kiddo@users.noreply.github.com>
2022-03-06 23:49:25 +0530

    Create pt05.rs

---
+++ 05 Primitive Types/pt05.rs
@@ -0,0 +1,8 @@
+// Code by Krypto Kiddo
+// Primitive Types Ex 5 : Destructuring Tuples 
+
+fn main(){
+    let cat = ("Furry McFurson", 3.5);
+    let (name,age) = cat;
+    println!("{} is {} years old",name,age);
+}
